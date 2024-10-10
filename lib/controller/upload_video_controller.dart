48c28caa9f6defb779d4a58b72f5e291f8303a8d
Amidoukone <koneamidou27@gmail.com>
2024-10-10 10:49:49 +0000

    Completing Offres and Evenements features. Now they are almost done

--- lib/controller/upload_video_controller.dart
+++ lib/controller/upload_video_controller.dart
@@ -3,10 +3,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:path/path.dart';
-import 'package:show_talent/controller/user_controller.dart'; // Pour obtenir l'utilisateur actuel
+import 'package:show_talent/controller/user_controller.dart';
 
 class UploadVideoController extends GetxController {
   var isUploading = false.obs;
+  var uploadProgress = 0.0.obs; // Progression du téléversement
 
   // Méthode pour téléverser une vidéo
   Future<void> uploadVideo(String songName, String caption, String videoPath) async {
@@ -23,36 +24,40 @@ class UploadVideoController extends GetxController {
       String fileName = basename(videoPath);
       Reference storageRef = FirebaseStorage.instance.ref().child('videos/$fileName');
 
-      // Téléverser la vidéo dans Firebase Storage
+      // Téléverser la vidéo dans Firebase Storage et écouter la progression
       UploadTask uploadTask = storageRef.putFile(videoFile);
+      uploadTask.snapshotEvents.listen((TaskSnapshot snapshot) {
+        uploadProgress.value = (snapshot.bytesTransferred / snapshot.totalBytes);  // Mise à jour de la progression
+      });
+
       TaskSnapshot snapshot = await uploadTask;
 
-      // Récupérer l'URL de la vidéo depuis Firebase Storage
+      // Récupérer l'URL de la vidéo
       String videoUrl = await snapshot.ref.getDownloadURL();
-      print('URL de la vidéo téléchargée : $videoUrl'); // Log pour vérifier l'URL
+      print('URL de la vidéo téléchargée : $videoUrl');
 
       // Générer un ID unique pour la vidéo dans Firestore
       String videoId = FirebaseFirestore.instance.collection('videos').doc().id;
 
       // Sauvegarder les métadonnées de la vidéo dans Firestore
       await FirebaseFirestore.instance.collection('videos').doc(videoId).set({
-        'id': videoId,  // Enregistrer l'ID unique de la vidéo
+        'id': videoId,
         'videoUrl': videoUrl,
         'songName': songName,
         'caption': caption,
         'likes': [],
         'shareCount': 0,
         'uid': Get.find<UserController>().user?.uid,  // ID de l'utilisateur
-        'thumbnail': '',  // Si vous avez un générateur de miniatures, gérez-le ici
-        'createdAt': FieldValue.serverTimestamp(),  // Date de création
+        'thumbnail': '',
+        'createdAt': FieldValue.serverTimestamp(),
       });
 
       Get.snackbar('Succès', 'Vidéo téléchargée avec succès !');
     } catch (e) {
-      print('Erreur lors du téléchargement de la vidéo : $e'); // Log pour les erreurs
       Get.snackbar('Erreur', 'Une erreur est survenue pendant le téléchargement : $e');
     } finally {
       isUploading(false);  // Indique la fin du téléchargement
+      uploadProgress.value = 0.0;
     }
   }
 }
--- lib/screens/upload_form.dart
+++ lib/screens/upload_form.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:show_talent/controller/upload_video_controller.dart';
+import 'package:show_talent/screens/home_screen.dart'; // Importer l'écran d'accueil
 
 class UploadForm extends StatefulWidget {
   final File videoFile;
@@ -18,12 +19,57 @@ class _UploadFormState extends State<UploadForm> {
   final TextEditingController songController = TextEditingController();
   final TextEditingController captionController = TextEditingController();
 
+  void showProgressDialog() {
+    Get.dialog(
+      Obx(() {
+        double progressValue = uploadVideoController.uploadProgress.value;
+        return AlertDialog(
+          title: const Text(
+            "Téléversement en cours...",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              LinearProgressIndicator(
+                value: progressValue,
+                backgroundColor: Colors.grey[300],
+                color: const Color(0xFF214D4F),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "${(progressValue * 100).toStringAsFixed(2)}% terminé",
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (!uploadVideoController.isUploading.value) {
+                  Get.back();  // Fermer la boîte de dialogue
+                  Get.offAll(() => const HomeScreen());  // Rediriger vers la page Home
+                }
+              },
+              child: const Text(
+                "Fermer",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        );
+      }),
+      barrierDismissible: false,  // Empêcher la fermeture pendant le téléversement
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: const Text('Téléverser une vidéo'),
+        backgroundColor: const Color(0xFF214D4F),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -31,32 +77,66 @@ class _UploadFormState extends State<UploadForm> {
           children: [
             TextField(
               controller: songController,
-              decoration: const InputDecoration(labelText: 'Nom de la chanson'),
+              decoration: const InputDecoration(
+                labelText: 'Description de la vidéo',
+                labelStyle: TextStyle(color: Colors.white),
+                filled: true,
+                fillColor: Color(0xFF2E2E2E),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+              style: const TextStyle(color: Colors.white),
             ),
             const SizedBox(height: 10),
             TextField(
               controller: captionController,
-              decoration: const InputDecoration(labelText: 'Légende'),
+              decoration: const InputDecoration(
+                labelText: 'Légende',
+                labelStyle: TextStyle(color: Colors.white),
+                filled: true,
+                fillColor: Color(0xFF2E2E2E),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+              style: const TextStyle(color: Colors.white),
             ),
             const SizedBox(height: 20),
             Obx(() {
               if (uploadVideoController.isUploading.value) {
-                return const CircularProgressIndicator();
+                return const CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF214D4F)),
+                );  // Afficher un indicateur si en train de téléverser
               }
               return ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF214D4F),
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                ),
                 onPressed: () {
                   if (songController.text.isNotEmpty && captionController.text.isNotEmpty) {
+                    showProgressDialog();  // Affiche le dialogue de progression
+
                     uploadVideoController.uploadVideo(
                       songController.text,
                       captionController.text,
                       widget.videoPath,
                     );
-                    Get.back(); // Retour après téléversement
                   } else {
-                    Get.snackbar('Erreur', 'Veuillez remplir tous les champs');
+                    Get.snackbar(
+                      'Erreur',
+                      'Veuillez remplir tous les champs',
+                      snackPosition: SnackPosition.BOTTOM,
+                      backgroundColor: Colors.redAccent,
+                      colorText: Colors.white,
+                    );
                   }
                 },
-                child: const Text('Téléverser la vidéo'),
+                child: const Text(
+                  'Téléverser la vidéo',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
               );
             }),
           ],
