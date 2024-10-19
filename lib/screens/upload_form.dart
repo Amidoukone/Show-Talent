84f61017d43d48b91c5893fa1a186ee76bba12ca
Amidoukone <koneamidou27@gmail.com>
2024-10-19 15:09:11 +0000

    Notification Ok for one user

--- lib/screens/upload_form.dart
+++ lib/screens/upload_form.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:show_talent/controller/push_notification.dart';
 import 'package:show_talent/controller/upload_video_controller.dart';
 import 'package:show_talent/screens/home_screen.dart'; // Importer l'écran d'accueil
 
@@ -8,14 +9,16 @@ class UploadForm extends StatefulWidget {
   final File videoFile;
   final String videoPath;
 
-  const UploadForm({super.key, required this.videoFile, required this.videoPath});
+  const UploadForm(
+      {super.key, required this.videoFile, required this.videoPath});
 
   @override
   State<UploadForm> createState() => _UploadFormState();
 }
 
 class _UploadFormState extends State<UploadForm> {
-  final UploadVideoController uploadVideoController = Get.put(UploadVideoController());
+  final UploadVideoController uploadVideoController =
+      Get.put(UploadVideoController());
   final TextEditingController songController = TextEditingController();
   final TextEditingController captionController = TextEditingController();
 
@@ -47,8 +50,9 @@ class _UploadFormState extends State<UploadForm> {
             TextButton(
               onPressed: () {
                 if (!uploadVideoController.isUploading.value) {
-                  Get.back();  // Fermer la boîte de dialogue
-                  Get.offAll(() => const HomeScreen());  // Rediriger vers la page Home
+                  Get.back(); // Fermer la boîte de dialogue
+                  Get.offAll(
+                      () => const HomeScreen()); // Rediriger vers la page Home
                 }
               },
               child: const Text(
@@ -59,7 +63,8 @@ class _UploadFormState extends State<UploadForm> {
           ],
         );
       }),
-      barrierDismissible: false,  // Empêcher la fermeture pendant le téléversement
+      barrierDismissible:
+          false, // Empêcher la fermeture pendant le téléversement
     );
   }
 
@@ -107,21 +112,34 @@ class _UploadFormState extends State<UploadForm> {
               if (uploadVideoController.isUploading.value) {
                 return const CircularProgressIndicator(
                   valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF214D4F)),
-                );  // Afficher un indicateur si en train de téléverser
+                ); // Afficher un indicateur si en train de téléverser
               }
               return ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color(0xFF214D4F),
-                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
                 ),
                 onPressed: () {
-                  if (songController.text.isNotEmpty && captionController.text.isNotEmpty) {
-                    showProgressDialog();  // Affiche le dialogue de progression
+                  if (songController.text.isNotEmpty &&
+                      captionController.text.isNotEmpty) {
+                    showProgressDialog(); // Affiche le dialogue de progression
 
                     uploadVideoController.uploadVideo(
                       songController.text,
                       captionController.text,
                       widget.videoPath,
+                    );
+
+                    String token =
+                        "eYdOU1VrQ-2EGLg8MlgKTs:APA91bGB2ZdJ3lSo3t5ynpJZY44HrAaEAVLipvKhEzS3NyaVynTRSc_Wi2YNvmIDc7URlOmL8o8V6iz8vNTYw9XyUhakhE3CAT-dAvjRjenWH_SEemERxZfXc0AVUm0MbIU8ShwL_gMb";
+
+                    PushNotificationService.sendNotification(
+                      title: "Nouvelle vidéo uploader",
+                      body: "jfnjidifjnf",
+                      token: token,
+                      contextType: "fhjdf",
+                      contextData: widget.videoPath,
                     );
                   } else {
                     Get.snackbar(
