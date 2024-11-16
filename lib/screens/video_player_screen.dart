1baa13f9839c12ab79cfc8389d93a3e8c431b589
Amidoukone <koneamidou27@gmail.com>
2024-11-16 13:15:49 +0000

    Fixing some issues with profil

---
+++ lib/screens/video_player_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:show_talent/models/video.dart';
+import 'package:show_talent/screens/video_player_item.dart';
+
+class VideoPlayerScreen extends StatelessWidget {
+  final String videoUrl;
+
+  const VideoPlayerScreen({super.key, required this.videoUrl, required Video video});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vidéo'),
+        backgroundColor: const Color(0xFF214D4F),
+      ),
+      body: Center(
+        child: VideoPlayerItem(videoUrl: videoUrl),
+      ),
+    );
+  }
+}
